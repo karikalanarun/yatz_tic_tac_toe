60550e556fd7a1d428e93048b40d21c1a9adb140
Arun Karikalan <arun.k@skitter.in>
2021-06-12 11:16:38 +0530

    row check added

--- scripts/tic_tac_toe.js
+++ scripts/tic_tac_toe.js
@@ -1,10 +1,29 @@
 
 
+let activePlayer = "X"
 
-function cellClicked(cellId) {
+let count = 0;
+
+
+function cellClicked(x, y) {
+    let cellId = `cell-${x}-${y}`;
     let cell = document.getElementById(cellId)
     let playerSymbol = document.createElement("div")
-    playerSymbol.innerText = "X";
+    playerSymbol.innerText = activePlayer;
     playerSymbol.classList.add("player-symbol")
     cell.append(playerSymbol)
+    let rowFirstElement = document.getElementById(`cell-${x}-${0}`)
+    let rowSecondElement = document.getElementById(`cell-${x}-${1}`)
+    let rowThirdElement = document.getElementById(`cell-${x}-${2}`)
+    let rowFirstElementSymbol = rowFirstElement.children.length !== 0 ? rowFirstElement.children[0].innerText : "";
+    let rowSecondElementSymbol = rowSecondElement.children.length !== 0 ? rowSecondElement.children[0].innerText : "";
+    let rowThirdElementSymbol = rowThirdElement.children.length !== 0 ? rowThirdElement.children[0].innerText : "";
+    if (((rowFirstElementSymbol === activePlayer) && (rowSecondElementSymbol === activePlayer)) && (rowThirdElementSymbol === activePlayer)) {
+        alert(`${activePlayer} is won`)
+    }
+    count = count + 1;
+    if (count === 9) {
+        alert("match draw")
+    }
+    activePlayer = activePlayer === "X" ? "O" : "X";
 }
